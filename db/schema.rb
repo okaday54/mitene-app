dce129e966306bda452761eac2a32316cdfd527b
okaday54 <okaday2020@gmail.com>
2020-05-01 23:09:28 +0900

    デバイスの修正(username,ログイン画面,新規登録)

--- db/schema.rb
+++ db/schema.rb
@@ -27,14 +27,14 @@ ActiveRecord::Schema.define(version: 2020_03_26_072348) do
   end
 
   create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.string "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
