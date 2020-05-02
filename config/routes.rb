9159f65ded929c345e1c6ef39ad8a1a00be59d54
okaday54 <okaday2020@gmail.com>
2020-05-02 22:14:55 +0900

    マイページ導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
+  
   root "posts#index"
   resources :posts do
     resources :comments, only: :create
