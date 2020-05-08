7ad115c62a2a955dbf3c06dc851778432212be79
okaday54 <okaday2020@gmail.com>
2020-05-08 23:22:05 +0900

    aws再設定

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,16 +6,12 @@ worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
 working_directory app_path
-pid "#{app_path}/tmp/pids/unicorn.pid"
-listen "#{app_path}/tmp/sockets/unicorn.sock"
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen 3000
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
